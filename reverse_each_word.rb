2b34a4ebb23cc4f1cff7c805bc6dcc1d03c07aa8
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-07-05 15:44:53 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,8 @@
 require 'spec_helper'
 
-def reverse_each_word(sentence)
-  
-  sentence.reverse{}
-  
-  
-
+def reverse_each str
+  arr = str.split /\b/
+  new_arr = arr.collect {|a| a.reverse}
+  new_arr.join
 end
-
   
