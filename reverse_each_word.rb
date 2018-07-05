8eb38761a04730530344b44c12c9e205ed56801b
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-07-05 15:37:06 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+def reverse_each_word 
+  let(:sentence1) { "Hello there, and how are you?" }
+  it 'reverses all the words in a string without reversing the order of the words' do
+    expect(reverse_each_word(sentence1)).to eq("olleH ,ereht dna woh era ?uoy")
+end
+
+  
